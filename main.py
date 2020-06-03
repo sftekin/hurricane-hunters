c651ab8f772815ff2980288e67eba066d4a16e60
selim <sftekin25@gmail.com>
2020-06-03 14:37:34 +0300

    mode implemented

--- main.py
+++ main.py
@@ -34,7 +34,10 @@ def select_best_model(results_dir):
     return best_model, best_conf
 
 
-def main(overwrite_flag):
+def main(args):
+    mode = args.mode
+    overwrite_flag = args.overwrite_flag
+
     model_name = 'trajgru'
     data_folder = 'data'
     hurricane_path = os.path.join(data_folder, 'ibtracs.NA.list.v04r00.csv')
@@ -44,45 +47,50 @@ def main(overwrite_flag):
     data = DataCreator(hurricane_path, **config_obj.data_params)
     hurricane_list, weather_list = data.hurricane_list, data.weather_list
 
-    print("Starting experiments")
-    for exp_count, conf in enumerate(config_obj.conf_list):
-        print('\nExperiment {}'.format(exp_count))
-        print('-*-' * 10)
+    if mode == 'train':
+        print("Starting experiments")
+        for exp_count, conf in enumerate(config_obj.conf_list):
+            print('\nExperiment {}'.format(exp_count))
+            print('-*-' * 10)
+
+            batch_generator = BatchGenerator(hurricane_list=hurricane_list,
+                                             weather_list=weather_list,
+                                             batch_size=conf["batch_size"],
+                                             window_len=conf["window_len"],
+                                             phase_shift=conf["phase_shift"],
+                                             return_mode=conf['return_mode'],
+                                             cut_start=conf['cut_start'],
+                                             vector_mode=conf['vector_mode'],
+                                             vector_freq=conf['vector_freq'],
+                                             **config_obj.experiment_params)
+
+            train(model_name, batch_generator, exp_count, overwrite_flag, **conf)
+
+    elif mode == 'test':
+        best_model, best_conf = select_best_model(results_folder)
 
         batch_generator = BatchGenerator(hurricane_list=hurricane_list,
                                          weather_list=weather_list,
-                                         batch_size=conf["batch_size"],
-                                         window_len=conf["window_len"],
-                                         phase_shift=conf["phase_shift"],
-                                         return_mode=conf['return_mode'],
-                                         cut_start=conf['cut_start'],
-                                         vector_mode=conf['vector_mode'],
-                                         vector_freq=conf['vector_freq'],
+                                         batch_size=best_conf["batch_size"],
+                                         window_len=best_conf["window_len"],
+                                         phase_sift=best_conf["phase_shift"],
+                                         return_mode=best_conf['return_mode'],
+                                         cut_start=best_conf['cut_start'],
+                                         vector_mode=best_conf['vector_mode'],
+                                         vector_freq=best_conf['vector_freq'],
                                          **config_obj.experiment_params)
 
-        train(model_name, batch_generator, exp_count, overwrite_flag, **conf)
+        print("Testing with best model...")
+        predict(best_model, batch_generator)
 
-    best_model, best_conf = select_best_model(results_folder)
-
-    batch_generator = BatchGenerator(hurricane_list=hurricane_list,
-                                     weather_list=weather_list,
-                                     batch_size=best_conf["batch_size"],
-                                     window_len=best_conf["window_len"],
-                                     phase_sift=best_conf["phase_shift"],
-                                     return_mode=best_conf['return_mode'],
-                                     cut_start=best_conf['cut_start'],
-                                     vector_mode=best_conf['vector_mode'],
-                                     vector_freq=best_conf['vector_freq'],
-                                     **config_obj.experiment_params)
-
-    print("Testing with best model...")
-    predict(best_model, batch_generator)
+    else:
+        raise ValueError('input mode: {} is not found'.format(mode))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--overwrite', type=int, default=0)  # overwrite previous results
+    parser.add_argument('--mode', type=str, default='train')
 
     args = parser.parse_args()
-
-    main(args.overwrite)
+    main(args)
